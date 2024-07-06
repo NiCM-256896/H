ee1b572421a9a17d9d2f19f68821fcaca15cee32
NiCM-256896 <nguyenhuuphuoctb2011@gmail.com>
2024-07-06 11:54:43 +0000

    php

---
+++ interface I {.ts
@@ -0,0 +1,11 @@
+interface I {
+        // We may naively assume that the PHP constant is always a string.
+            const PHP = 'PHP 8.2';
+}
+
+class Foo implements I {
+        // But implementing classes may define it as an array.
+            const PHP = [];
+}
+}
+}
